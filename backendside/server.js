9dcd2b6451173c1deb09187a69ddcdd7cdfc775c
Ekemiben <ekemiben.4@gmail.com>
2022-09-22 18:19:07 +0100

    closing commit

--- backendside/server.js
+++ backendside/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const routesUrl = require('./routes/routes')
+const cors = require('cors')
 
 dotenv.config
 
-mongoose.connect();
+mongoose.connect(process.env.DATABASE_URI, () => console.log("Database connected"));
 
+// This is bodyPaser
+app.use(express.json());
+app.use(cors());
 
-app.listen(4000, () => console.log("Server is runing on prot 4000"))
+app.use('/app', routesUrl)
+app.listen(4000, () => console.log("Server is runing on port 4000"))
